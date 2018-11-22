93fedd3817f4ad378fcefae3614ed219929663a6
malavika <malavika.akhilesh@titechnologies.org>
2018-11-22 14:38:13 +0530

    Initial Commit

---
+++ Store@HomeUITests/Store_HomeUITests.swift
@@ -0,0 +1,36 @@
+//
+//  Store_HomeUITests.swift
+//  Store@HomeUITests
+//
+//  Created by MALAVIKA on 22/11/18.
+//  Copyright © 2018 MALAVIKA. All rights reserved.
+//
+
+import XCTest
+
+class Store_HomeUITests: XCTestCase {
+        
+    override func setUp() {
+        super.setUp()
+        
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+        
+        // In UI tests it is usually best to stop immediately when a failure occurs.
+        continueAfterFailure = false
+        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
+        XCUIApplication().launch()
+
+        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+}
