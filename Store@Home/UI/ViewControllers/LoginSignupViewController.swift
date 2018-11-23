4485ff46b44f81c8c4c9ad9e15f9f0c025f802ad
malavika <malavika.akhilesh@titechnologies.org>
2018-11-23 16:42:16 +0530

    Login UI & partial functionality

--- Store@Home/UI/ViewControllers/LoginSignupViewController.swift
+++ Store@Home/UI/ViewControllers/LoginSignupViewController.swift
@@ -9,13 +9,68 @@
 import UIKit
 
 class LoginSignupViewController: UIViewController {
-
+    @IBOutlet weak var loginView: UIView!
+    @IBOutlet weak var signupView: UIView!
+    @IBOutlet weak var loginEmailTextField: FloatLabelTextField!
+    @IBOutlet weak var loginPasswordTextField: FloatLabelTextField!
+    @IBOutlet weak var firstNameTextField: FloatLabelTextField!
+    @IBOutlet weak var lastNameTextField: FloatLabelTextField!
+    @IBOutlet weak var emailTextField: FloatLabelTextField!
+    @IBOutlet weak var passwordTextField: FloatLabelTextField!
+    @IBOutlet weak var refcodeTextField: FloatLabelTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
+    
+    override func viewDidLayoutSubviews() {
+        (loginView.superview?.superview as! UIScrollView).contentSize = (loginView.superview?.frame.size)!
+    }
 
+    @IBAction func onClickClose(_ sender: UIButton) {
+        self.dismiss(animated: true, completion: nil)
+    }
+    
+    @IBAction func onChangeSegmentValue(_ sender: UISegmentedControl) {
+        if sender.selectedSegmentIndex == 0 {
+            loginView.isHidden = false
+            signupView.isHidden = true
+        } else {
+            signupView.isHidden = false
+            loginView.isHidden = true
+        }
+    }
+    
+    @IBAction func onClickFB(_ sender: UIButton) {
+    }
+    
+    @IBAction func onClickGooglePlus(_ sender: UIButton) {
+    }
+    
+    @IBAction func onClickForgotPassword(_ sender: UIButton) {
+    }
+    
+    @IBAction func onClickOTPLogin(_ sender: UIButton) {
+    }
+    
+    @IBAction func onClickLoginButton(_ sender: UIButton) {
+        onClickClose(sender)
+    }
+    @IBAction func onClickShowButton(_ sender: UIButton) {
+        sender.isSelected = !sender.isSelected
+        passwordTextField.isSecureTextEntry = sender.isSelected ? false : true
+    }
+    
+    @IBAction func onClickInfoButton(_ sender: UIButton) {
+    }
+    
+    @IBAction func onClickSignupButton(_ sender: UIButton) {
+        onClickClose(sender)
+    }
+    
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
