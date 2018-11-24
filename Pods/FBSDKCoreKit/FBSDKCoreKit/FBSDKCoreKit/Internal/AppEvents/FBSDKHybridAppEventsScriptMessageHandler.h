b6811a12a7572c781f73327914935e327fe7a077
malavika <malavika.akhilesh@titechnologies.org>
2018-11-24 11:51:48 +0530

    New files added on Pod

--- Pods/FBSDKCoreKit/FBSDKCoreKit/FBSDKCoreKit/Internal/AppEvents/FBSDKHybridAppEventsScriptMessageHandler.h
+++ Pods/FBSDKCoreKit/FBSDKCoreKit/FBSDKCoreKit/Internal/AppEvents/FBSDKHybridAppEventsScriptMessageHandler.h
@@ -16,15 +16,11 @@
 // IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
 // CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-#import "FBSDKDeviceLoginError.h"
+#if !TARGET_OS_TV
+#import <Foundation/Foundation.h>
+#import <WebKit/WebKit.h>
 
-#import "FBSDKLoginConstants.h"
-
-@implementation FBSDKDeviceLoginError
-
-+ (NSString *)errorDomain
-{
-  return FBSDKLoginErrorDomain;
-}
+@interface FBSDKHybridAppEventsScriptMessageHandler : NSObject <WKScriptMessageHandler>
 
 @end
+#endif
